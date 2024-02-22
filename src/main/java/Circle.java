8baaa36c41b7e2a9745dfb17a09a66b943f1685e
Kihangu Waweru <kw817373@gmail.com>
2024-02-22 15:38:12 -0500

    Recitation 3

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -17,7 +17,8 @@ public class Circle
      */
     public Circle()
     {
-
+        location = new Point(); // don't have to say this.location because there's nothing with the same name locally
+        radius = 1.0;
     }
 
     /**
@@ -28,7 +29,8 @@ public class Circle
      */
     public Circle(double radius)
     {
-
+        this.radius = radius;
+        location = new Point();
     }
 
     /**
@@ -40,7 +42,8 @@ public class Circle
      */
     public Circle(Point location, double radius)
     {
-
+        this.radius = radius;
+        this.location = new Point(location);
     }
 
     //accessors
@@ -51,7 +54,7 @@ public class Circle
      */
     public double getRadius()
     {
-
+        return radius;
     }
 
     /**
@@ -61,7 +64,7 @@ public class Circle
      */
     public Point getLocation()
     {
-
+        return new Point(location);
     }
 
     // mutator methods
@@ -72,7 +75,7 @@ public class Circle
      */
     public void setRadius(double radius)
     {
-
+        this.radius = radius;
     }
 
     /**
@@ -82,7 +85,7 @@ public class Circle
      */
     public void setLocation(Point location)
     {
-
+        this.location = new Point(location);
     }
 
     // other methods
@@ -94,7 +97,7 @@ public class Circle
      */
     public boolean contains(Point p)
     {
-
+        return p.distanceTo(location) <= radius;
     }
 
     /**
@@ -105,7 +108,7 @@ public class Circle
      */
     public boolean intersects(Circle aCircle)
     {
-
+        return location.distanceTo(aCircle.location) <= radius + aCircle.radius;
     }
 
     /**
--- src/main/java/Recitation3.java
+++ src/main/java/Recitation3.java
@@ -4,7 +4,17 @@ public class Recitation3 {
     public static void main(String[] argv)
     {
         Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        Circle circle16 = new Circle(new Point(x,y), 16.0);
+        Circle circle40 = new Circle(new Point(14,12), 40.0);
+        System.out.println("Enter x:");
+        System.out.println("Enter y:");
 
+        if (circle40.intersects(circle16)) {
+            System.out.println("Circles intersect!");
+        }else
+                System.out.println("Circles do not intersect!");
+        }
         // add your code here
     }
-}
